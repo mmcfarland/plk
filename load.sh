45286c8f6a812095f1670b7f3f46632253ed74b0
Matthew McFarland <mmcfarland@gmail.com>
2013-08-19 22:36:38 -0400

    Script to download and load pwd parcel data

---
+++ load.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+DB="dataviewer"
+
+# Download and unzip the pwd parcel shapefile
+cd /tmp/
+rm -rf pwd_parcels_workspace
+mkdir pwd_parcels_workspace && cd pwd_parcels_workspace
+wget http://gis.phila.gov/data/PARCELS_PWD.zip
+unzip PARCELS_PWD.zip
+
+# Load the shapefile into postgis
+shp2pgsql -I -D -s 2272 PARCELS_PWD.shp pwd_parcels > p.sql
+psql -d $DB -c "drop table pwd_parcels;"
+psql -d $DB -f p.sql
+
+psql -d $DB << EOF
+    create index pwd_parcelid_idx on pwd_parcels (parcelid);
+    create index pwd_opaid_idx on pwd_parcels (brt_id);
+    alter table pwd_parcels alter column geom set data type geometry(Multipolygon,4326) using ST_Transform(geom, 4326);
+    update pwd_parcels set geom = ST_MakeValid(geom) where ST_IsValid(geom) = false;
+    alter table pwd_parcels add column pos geometry (Point, 4326);
+    update pwd_parcels set pos = ST_PointOnSurface(geom);
+EOF
+
+   
+ls
