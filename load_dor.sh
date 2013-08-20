6d1b9d05f86b805ff3257e4459abaac9ef34cca3
Matthew McFarland <mmcfarland@gmail.com>
2013-08-19 22:37:05 -0400

    Script to download and load DOR parcel data

---
+++ load_dor.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+DB="dataviewer"
+
+# Download and unzip the dor parcel shapefile
+cd /tmp/
+rm -rf dor_parcels_workspace
+mkdir dor_parcels_workspace && cd dor_parcels_workspace
+wget http://www.pasda.psu.edu/philacity/data/Philadelphia_DOR_Parcels_Active201302.zip 
+unzip Philadelphia_DOR_Parcles_Active201302.zip
+
+# Load the shapefile into postgis
+shp2pgsql -I -D -s 2272 Philadelphia_DOR_Parcles_Active201302.shp plk_dor_parcels > p.sql
+psql -d $DB -c "drop table plk_dor_parcels;"
+psql -d $DB -f p.sql
+
+psql -d $DB << EOF
+    create index dor_parcelid_idx on plk_dor_parcels (parcelid);
+    create index dor_basereg_idx on plk_dor_parcels (basereg);
+    alter table plk_dor_parcels alter column geom set data type geometry(Multipolygon,4326) using ST_Transform(geom, 4326);
+    update plk_dor_parcels set geom = ST_MakeValid(geom) where ST_IsValid(geom) = false;
+    alter table plk_dor_parcels add column pos geometry (Point, 4326);
+    update plk_dor_parcels set pos = ST_PointOnSurface(geom);
+EOF
+
+   
+ls
