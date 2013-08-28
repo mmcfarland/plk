f8b104c91840cbb82e9bddc9c3dae2577588d064
Matthew McFarland <mmcfarland@gmail.com>
2013-08-27 21:36:14 -0400

    Query by opa/regmap, full records returned

--- plk.go
+++ plk.go
@@ -15,7 +15,10 @@ import (
 
 type Hdl func(w http.ResponseWriter, r *http.Request)
 type Parcel struct {
-	OpaNum int `json:"opa"`
+	OpaNum      string `json:"opa"`
+	PwdParcelId int    `json:"pwdParcelId"`
+	Mapreg      string `json:"mapreg"`
+	Address     string `json:"address"`
 }
 
 type Config struct {
@@ -50,6 +53,17 @@ func ParcelMarshal(w http.ResponseWriter, p *Parcel, err error) {
 	w.Write(b)
 }
 
+func ParcelsMarshal(w http.ResponseWriter, p []Parcel, err error) {
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Could not find parcel", 404)
+		return
+	}
+
+	b, _ := json.Marshal(p)
+	w.Write(b)
+}
+
 func ByCoords(w http.ResponseWriter, r *http.Request) {
 	lat, err := strconv.ParseFloat(r.FormValue("lat"), 32)
 	if err != nil {
@@ -63,17 +77,68 @@ func ByCoords(w http.ResponseWriter, r *http.Request) {
 	}
 
 	pt := fmt.Sprintf("POINT (%f %f)", lon, lat)
-	sql := `SELECT brt_id 
-            FROM pwd_parcels
-            WHERE ST_Intersects(ST_GeomFromText($1, 4326), geom) = true;`
+	sql := `SELECT brt_id, parcelid, mapreg, address 
+            FROM plk
+            WHERE ST_Intersects(ST_GeomFromText($1, 4326), pwd_geom) = true;`
 	s, err := DbConn.Prepare(sql)
-	var p Parcel
-	fmt.Println(pt)
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = s.QueryRow(pt).Scan(&p.OpaNum)
-	ParcelMarshal(w, &p, err)
+	p, err := ScanParcelRow(s.QueryRow(pt))
+	ParcelMarshal(w, p, err)
+}
+
+func ByRegMap(w http.ResponseWriter, r *http.Request) {
+	regmap := r.FormValue("regmap")
+
+	sql := `SELECT brt_id, parcelid, mapreg, address 
+            FROM plk
+            WHERE basereg = $1;`
+	ByValueLookup(w, r, sql, regmap)
+}
+
+func ByOpa(w http.ResponseWriter, r *http.Request) {
+	opa := r.FormValue("opa")
+
+	sql := `SELECT brt_id, parcelid, mapreg, address 
+            FROM plk
+            WHERE brt_id = $1;`
+	ByValueLookup(w, r, sql, opa)
+}
+
+func ByValueLookup(w http.ResponseWriter, r *http.Request, sql string, val string) {
+
+	s, err := DbConn.Prepare(sql)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	rs, err := s.Query(val)
+	p, err := ScanParcelRows(*rs)
+	ParcelsMarshal(w, p, err)
+}
+
+type Scanner interface {
+	Scan(dest ...interface{}) error
+}
+
+func ScanParcelRow(s Scanner) (*Parcel, error) {
+	var p Parcel
+	err := s.Scan(&p.OpaNum, &p.PwdParcelId, &p.Mapreg, &p.Address)
+	return &p, err
+}
+
+func ScanParcelRows(rs sql.Rows) ([]Parcel, error) {
+	var parcels []Parcel
+	for rs.Next() {
+		p, err := ScanParcelRow(&rs)
+		if err != nil {
+			return nil, err
+		} else {
+			parcels = append(parcels, *p)
+		}
+	}
+	return parcels, nil
 }
 
 func main() {
@@ -93,9 +158,9 @@ func main() {
 	api := r.PathPrefix("/api/v0.1").Subrouter()
 	api.StrictSlash(false)
 	api.HandleFunc("/parcel/", ByCoords).Queries("lat", "", "lon", "")
+	api.HandleFunc("/parcel/", ByRegMap).Queries("regmap", "")
+	api.HandleFunc("/parcel/", ByOpa).Queries("opa", "")
 	//api.Handle("/parcel/", ByAddress).Queries("address", "")
-	//api.Handle("/parcel/", ByRegMap).Queries("regmap", "")
-	//api.Handle("/parcel/", ByOpa).Queries("opa", "")
 
 	http.Handle("/", r)
 	p := strconv.Itoa(*port)
